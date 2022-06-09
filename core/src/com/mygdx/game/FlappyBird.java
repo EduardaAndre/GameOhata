191fa5e290c9b1bbfbfac3b35058f284a77e7965
EduardaAndre <69465415+EduardaAndre@users.noreply.github.com>
2022-06-09 20:31:37 -0300

    Comentário do código

--- core/src/com/mygdx/game/FlappyBird.java
+++ core/src/com/mygdx/game/FlappyBird.java
@@ -21,7 +21,7 @@ import java.util.Random;
 
 public class FlappyBird extends ApplicationAdapter {
 
-    private SpriteBatch batch;
+    private SpriteBatch batch;//Classe responsável pelo desenho da Sprite(Sprite Batch).
     private Texture[] passaros;
     private Texture fundo;
     private Texture canoBaixo;
@@ -33,13 +33,13 @@ public class FlappyBird extends ApplicationAdapter {
     private Texture titulo;
 
 
-    private ShapeRenderer shapeRenderer;
+    private ShapeRenderer shapeRenderer;//Classe responsável pela colisão.
     private Circle circuloPassaro;
     private Circle circuloMoeda;
     private Rectangle retanguloCanoCima;
     private Rectangle retanguloCanoBaixo;
 
-    private float larguraDispositivo;
+    private float larguraDispositivo;//classes na qual define o espaçamento da tela.
     private float alturaDispositivo;
     private float variacao = 0;
     private float gravidade = 2;
@@ -58,24 +58,24 @@ public class FlappyBird extends ApplicationAdapter {
     private float posicaoMoedaHorizontal;
     private float scaleMoeda = .2f;
 
-    BitmapFont textoPontuacao;
+    BitmapFont textoPontuacao;//mapeamento da fonte
     BitmapFont textoReiniciar;
     BitmapFont textoMelhorPontuacao;
 
-    Sound somVoando;
+    Sound somVoando;//Som do jogo 
     Sound somColisao;
     Sound somPontuacao;
     Sound moedaSom;
 
-    Preferences preferencias;
+    Preferences preferencias;//preferencias
 
-    private OrthographicCamera camera;
+    private OrthographicCamera camera;//Classe camera do jogo
     private Viewport viewport;
     private final float VIRTUAL_WIDTH = 720;
     private final float VIRTUAL_HEIGHT = 1280;
 
     @Override
-    public void create()
+    public void create()//Aqui é onde inicia o programa que faz com que inicia as Texturas e Objetos.
     {
         inicializarTexturas();
         inicializaObjetos();
@@ -83,7 +83,7 @@ public class FlappyBird extends ApplicationAdapter {
 
 
     @Override
-    public void render()
+    public void render()//Update(chama cada frame).
     {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
@@ -94,7 +94,7 @@ public class FlappyBird extends ApplicationAdapter {
     }
 
 
-    private void inicializarTexturas()
+    private void inicializarTexturas()//Onde é definida as texturas aparente no jogo. 
     {
         passaros = new Texture[3];
         passaros[0] = new Texture("passaro1.png");
@@ -113,10 +113,10 @@ public class FlappyBird extends ApplicationAdapter {
         titulo = new Texture("icone_lambu.png");
     }
 
-    private void inicializaObjetos()
+    private void inicializaObjetos()//Onde é criado a inicizalização dos Objetos.
     {
-        batch = new SpriteBatch();
-        random = new Random();
+        batch = new SpriteBatch();//desenho da Sprite
+        random = new Random();//seria tipo uma classe na qual é definido um valor aleatorio.
 
         larguraDispositivo = VIRTUAL_WIDTH;
         alturaDispositivo = VIRTUAL_HEIGHT;
@@ -124,43 +124,43 @@ public class FlappyBird extends ApplicationAdapter {
         posicaoCanoHorizontal = larguraDispositivo;
         posicaoMoedaHorizontal = posicaoCanoHorizontal + larguraDispositivo / 2;
         posicaoMoedaVertical = alturaDispositivo / 2;
-        espacoEntreCanos = 350;
+        espacoEntreCanos = 350;//Aqui é onde é calculado o passaro com o espaçamento do dispositivo, para ficar adequado ao jogo.
 
         textoPontuacao = new BitmapFont();
         textoPontuacao.setColor(Color.WHITE);
-        textoPontuacao.getData().setScale(10);
+        textoPontuacao.getData().setScale(10);//aqui é onde é feito o Bitmap da fonte(mapeamento da fonte), e onde é definido a cor da pontuação do texto.
 
         textoReiniciar = new BitmapFont();
         textoReiniciar.setColor(Color.GREEN);
-        textoReiniciar.getData().setScale(2);
+        textoReiniciar.getData().setScale(2);//feito o mapeamento do texto reiniciar, em cor verde.
 
         textoMelhorPontuacao = new BitmapFont();
         textoMelhorPontuacao.setColor(Color.RED);
-        textoMelhorPontuacao.getData().setScale(2);
+        textoMelhorPontuacao.getData().setScale(2);//feito o mapeamento do texto melhor pontuação, em cor vermelha.
 
         shapeRenderer = new ShapeRenderer();
         circuloPassaro = new Circle();
         circuloMoeda = new Circle();
         retanguloCanoBaixo = new Rectangle();
-        retanguloCanoCima = new Rectangle();
+        retanguloCanoCima = new Rectangle();//Aqui é definido o collider, o colisor.
 
         somVoando = Gdx.audio.newSound(Gdx.files.internal("som_asa.wav"));
         somColisao = Gdx.audio.newSound(Gdx.files.internal("som_batida.wav"));
         somPontuacao = Gdx.audio.newSound(Gdx.files.internal("som_pontos.wav"));
-        moedaSom = Gdx.audio.newSound(Gdx.files.internal("som_moeda.wav"));
+        moedaSom = Gdx.audio.newSound(Gdx.files.internal("som_moeda.wav"));//Aqui é onde é pego o som. Se for um arquivo .wav, ou se for outro tipo de arquivo.
 
         preferencias = Gdx.app.getPreferences("flappyBird");
-        pontuacaoMaxima = preferencias.getInteger("pontuacaoMaxima", 0);
+        pontuacaoMaxima = preferencias.getInteger("pontuacaoMaxima", 0);//aqui é onde é salvo no dispositivo.
 
         camera = new OrthographicCamera();
         camera.position.set(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 0);
-        viewport = new StretchViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, camera);
+        viewport = new StretchViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, camera);//aqui é onde medido a posição da camera.
     }
 
 
-    private void verificarEstadoJogo()
+    private void verificarEstadoJogo()//Aqui é onde é feito o mecanismo do jogo.
     {
-        boolean toqueTela = Gdx.input.justTouched();
+        boolean toqueTela = Gdx.input.justTouched();//Aqui é onde é definido que com o toque da tela conseguimos nos locomover.
 
         if (estadoJogo == 0)
         {
@@ -179,14 +179,14 @@ public class FlappyBird extends ApplicationAdapter {
                 somVoando.play();
             }
 
-            posicaoCanoHorizontal -= Gdx.graphics.getDeltaTime() * 200;
+            posicaoCanoHorizontal -= Gdx.graphics.getDeltaTime() * 200;//posição do cano
             posicaoMoedaHorizontal -=Gdx.graphics.getDeltaTime() * 200;
 
             if (posicaoCanoHorizontal < -canoTopo.getWidth())
             {
                 posicaoCanoHorizontal = larguraDispositivo;
                 posicaoCanoVertical = random.nextInt(400) - 200;
-                passouCano = false;
+                passouCano = false;//se o cano sai da tela, ele reseta a posição e pega uma altura aleatoria.
             }
 
             if(posicaoMoedaHorizontal < -moedaAtual.getWidth()*scaleMoeda /2)
@@ -194,9 +194,9 @@ public class FlappyBird extends ApplicationAdapter {
                 resetaMoeda();
             }
 
-            if (posicaoInicialVerticalPassaro > 0 || toqueTela) posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;
+            if (posicaoInicialVerticalPassaro > 0 || toqueTela) posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;//faz o passaro pular.
 
-            gravidade++;
+            gravidade++;//é a gravidade onde faz o passaro cair. 
         }
         else if (estadoJogo == 2)
         {
@@ -204,7 +204,7 @@ public class FlappyBird extends ApplicationAdapter {
             {
                 pontuacaoMaxima = pontos;
                 preferencias.putInteger("pontuacaoMaxima", pontuacaoMaxima);
-                preferencias.flush();
+                preferencias.flush();//Salva a pontuação do jogo.
             }
 
             posicaoHorizontalPassaro -= Gdx.graphics.getDeltaTime() * 500;
@@ -217,7 +217,7 @@ public class FlappyBird extends ApplicationAdapter {
                 posicaoHorizontalPassaro = 0;
                 posicaoInicialVerticalPassaro = alturaDispositivo / 2;
                 posicaoCanoHorizontal = larguraDispositivo;
-                resetaMoeda();
+                resetaMoeda();//Aqui é a tela inicial do jogo, onde é mostrado o passaro parado, sem pontuação.
             }
         }
     }
@@ -232,13 +232,13 @@ public class FlappyBird extends ApplicationAdapter {
     }
 
 
-    private void detectarColisoes()
+    private void detectarColisoes()//detecta as colisões.
     {
         circuloPassaro.set
         (
             50 + posicaoHorizontalPassaro + passaros[0].getWidth() * scalePassaro / 2,
             posicaoInicialVerticalPassaro + passaros[0].getHeight() * scalePassaro / 2,
-            passaros[0].getWidth() * scalePassaro / 2
+            passaros[0].getWidth() * scalePassaro / 2  //é definido a escala do passaro, a posição dele.
         );
 
         circuloMoeda.set
@@ -251,18 +251,18 @@ public class FlappyBird extends ApplicationAdapter {
         retanguloCanoBaixo.set(
                 posicaoCanoHorizontal,
                 alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + posicaoCanoVertical,
-                canoBaixo.getWidth(), canoBaixo.getHeight()
+                canoBaixo.getWidth(), canoBaixo.getHeight() //definição da posicão do cano debaixo, a altura, a largura.
 
         );
         retanguloCanoCima.set(
                 posicaoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + posicaoCanoVertical,
-                canoTopo.getWidth(), canoTopo.getHeight()
+                canoTopo.getWidth(), canoTopo.getHeight() //definição da posição do cano de cima, a altura e a largura. 
 
         );
 
         boolean colidiuCanoCima = Intersector.overlaps(circuloPassaro, retanguloCanoCima);
         boolean colidiuCanoBaixo = Intersector.overlaps(circuloPassaro, retanguloCanoBaixo);
-        boolean colidiuMoeda = Intersector.overlaps(circuloPassaro, circuloMoeda);
+        boolean colidiuMoeda = Intersector.overlaps(circuloPassaro, circuloMoeda); //O intersector.overlaps vai verificar a colisão, quando se passa o passaro entre os canos.
 
         if (colidiuCanoCima == true || colidiuCanoBaixo == true)
         {
@@ -271,7 +271,7 @@ public class FlappyBird extends ApplicationAdapter {
                 somColisao.play();
                 estadoJogo = 2;
             }
-        }
+        } //se colidiu com o cano em estado do jogo um, é tocado o som de colisão e muda o estado pra 2.
         if(colidiuMoeda == true)
         {
             if(moedaAtual == moedaOuro) pontos += 10;
@@ -284,8 +284,8 @@ public class FlappyBird extends ApplicationAdapter {
 
     private void desenharTexturas()
     {
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
+        batch.setProjectionMatrix(camera.combined);//posicionamento da camera.
+        batch.begin();//inicia os desenhos.
 
         batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
 
@@ -297,7 +297,7 @@ public class FlappyBird extends ApplicationAdapter {
                 larguraDispositivo / 2,
                 alturaDispositivo / 2
             );
-        }
+        } //Desenhado o titulo no estado 0 do jogo.
 
         batch.draw
         (
@@ -331,7 +331,7 @@ public class FlappyBird extends ApplicationAdapter {
             alturaDispositivo / 2 + espacoEntreCanos / 2 + posicaoCanoVertical
         );
 
-        textoPontuacao.draw(batch, String.valueOf(pontos), larguraDispositivo / 2, alturaDispositivo - 110);
+        textoPontuacao.draw(batch, String.valueOf(pontos), larguraDispositivo / 2, alturaDispositivo - 110);//Aqui é onde desenhado o titulo, os canos, a moeda do jogo.
 
 
         if (estadoJogo == 2)
@@ -360,7 +360,7 @@ public class FlappyBird extends ApplicationAdapter {
             );
         }
 
-        batch.end();
+        batch.end();//reune as texturas e renderiza elas
     }
 
 	public void validarPontos()
@@ -373,15 +373,15 @@ public class FlappyBird extends ApplicationAdapter {
     			passouCano = true;
     			somPontuacao.play();
 			}
-		}
+		}//Quando passar pelo cano, será tocado a musica e irá somar a pontuação. 
 
     	variacao += Gdx.graphics.getDeltaTime() *10;
 
     	if(variacao>3) variacao = 0;
-	}
+	}//reseta as variações dos passaros(a quantidade de passaros que é feito a animação).
 
     @Override
-    public void resize(int width, int height) { viewport.update(width, height); }
+    public void resize(int width, int height) { viewport.update(width, height); }//ele vai redimensiona a viewport com base na resolução do dispositivo. 
 
     @Override
     public void dispose() { }
